5e0beaaebe86c5eecb503402238b77f2a91c85df
Khayaeljuniormokoena <khayaeljuniormokoena>
2024-02-25 11:06:52 -0800

    update

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# 100-puppet_ssh_config.pp
+
+# Ensure SSH client configuration
+class { 'stdlib': }
+
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
+}
